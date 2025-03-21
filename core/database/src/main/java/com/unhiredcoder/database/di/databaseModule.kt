49999b5b9e7c8aa3129897a51f47a2ceb87c502d
PRATYUSH TIWARI <42827589+UnHired-Coder@users.noreply.github.com>
2025-03-21 11:16:07 +0530

    adds: database di module

---
+++ core/database/src/main/java/com/unhiredcoder/database/di/databaseModule.kt
@@ -0,0 +1,15 @@
+package com.unhiredcoder.database.di
+
+import com.unhiredcoder.database.MangaAppDatabase
+import com.unhiredcoder.database.dao.MangaDao
+import com.unhiredcoder.database.getMangaAppDatabase
+import org.koin.dsl.module
+
+val databaseModule = module {
+    single {
+        getMangaAppDatabase(get())
+    }
+    single<MangaDao> {
+        get<MangaAppDatabase>().mangaDao()
+    }
+}
