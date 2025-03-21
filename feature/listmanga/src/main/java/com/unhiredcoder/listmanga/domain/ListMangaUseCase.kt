d7876885cf249be83cab40064efcae7febf03633
PRATYUSH TIWARI <42827589+UnHired-Coder@users.noreply.github.com>
2025-03-21 11:24:11 +0530

    adds: ListMangaUseCase

---
+++ feature/listmanga/src/main/java/com/unhiredcoder/listmanga/domain/ListMangaUseCase.kt
@@ -0,0 +1,17 @@
+package com.unhiredcoder.listmanga.domain
+
+import com.unhiredcoder.listmanga.data.IMangaC
+import com.unhiredcoder.listmanga.ui.model.Manga
+import com.unhiredcoder.listmanga.ui.model.mapToManga
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class ListMangaUseCase(private val mangaRepository: IMangaC.Repository) {
+    operator fun invoke(): Flow<List<Manga>> {
+        return mangaRepository.getMangaList().map { mangaModelsList ->
+            mangaModelsList.map { mangaModel ->
+                mangaModel.mapToManga()
+            }
+        }
+    }
+}
---
+++ feature/listmanga/src/main/java/com/unhiredcoder/listmanga/domain/model/MangaModel.kt
@@ -0,0 +1,29 @@
+package com.unhiredcoder.listmanga.domain.model
+
+import com.unhiredcoder.database.model.MangaEntity
+
+data class MangaModel(
+    val id: String,
+    val imageUrl: String,
+    val score: Double,
+    val popularity: Long,
+    val title: String,
+    val publishedChapterDate: String,
+    val category: String,
+    val isFavourite: Boolean,
+    val isReadByUser: Boolean
+)
+
+fun MangaEntity.mapToMangaModel(): MangaModel {
+    return MangaModel(
+        id = id,
+        imageUrl = imageUrl,
+        score = score,
+        popularity = popularity,
+        title = title,
+        publishedChapterDate = publishedChapterDate,
+        category = category,
+        isFavourite = isFavourite,
+        isReadByUser = isReadByUser
+    )
+}
