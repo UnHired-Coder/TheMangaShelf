504f92d97c4d2bd45d9227fa45f998639eeb24ba
PRATYUSH TIWARI <42827589+UnHired-Coder@users.noreply.github.com>
2025-03-21 11:22:51 +0530

    implements: manga client local

---
+++ feature/listmanga/src/main/java/com/unhiredcoder/listmanga/data/local/MangaLocal.kt
@@ -0,0 +1,21 @@
+package com.unhiredcoder.listmanga.data.local
+
+import com.unhiredcoder.database.dao.MangaDao
+import com.unhiredcoder.database.model.MangaEntity
+import com.unhiredcoder.listmanga.data.IMangaC
+import kotlinx.coroutines.flow.Flow
+
+
+class MangaLocal(private val mangaDao: MangaDao) : IMangaC.Local {
+    override suspend fun updateMangaItem(mangaId: String, manga: MangaEntity):Boolean {
+        return mangaDao.updateMangaItem(mangaId = mangaId, manga = manga) > 0
+    }
+
+    override suspend fun updateMangaList(mangas: List<MangaEntity>) {
+        mangaDao.updateMangaList(mangas)
+    }
+
+    override fun getMangaList(): Flow<List<MangaEntity>> {
+        return mangaDao.getMangaList()
+    }
+}
