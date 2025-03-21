18afeb1b837e62f551d27e1798f0732844727311
PRATYUSH TIWARI <42827589+UnHired-Coder@users.noreply.github.com>
2025-03-22 00:21:05 +0530

    adds: DateSeparatorUI

---
+++ feature/listmanga/src/main/java/com/unhiredcoder/listmanga/ui/composables/DateSeparatorUI.kt
@@ -0,0 +1,44 @@
+package com.unhiredcoder.listmanga.ui.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun DateSeparatorUI(modifier: Modifier = Modifier, date: String) {
+    Box(
+        modifier =
+        modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        Spacer(
+            Modifier
+                .fillMaxWidth()
+                .height(0.5.dp)
+                .background(
+                    Color.Gray.copy(alpha = 0.5f), shape = RoundedCornerShape(50)
+                )
+        )
+
+        DatePillViewUI(date = date, isSelected = false, onDateSelected = {})
+    }
+}
+
+
+@Preview
+@Composable
+fun DateSeparatorUIPreview() {
+    DateSeparatorUI(date = "10-20-2010")
+}
