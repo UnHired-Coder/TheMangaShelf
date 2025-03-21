284fe134deb5ff1d2a3dbb81ad89b4c2191f9874
PRATYUSH TIWARI <42827589+UnHired-Coder@users.noreply.github.com>
2025-03-21 21:46:48 +0530

    fix: pass db context

--- core/database/src/main/java/com/unhiredcoder/database/DatabaseInstanceProvider.kt
+++ core/database/src/main/java/com/unhiredcoder/database/DatabaseInstanceProvider.kt
@@ -1,9 +1,9 @@
 package com.unhiredcoder.database
 
-import android.app.Application
+import android.content.Context
 import androidx.room.Room
 
-fun getMangaAppDatabase(applicationContext: Application): MangaAppDatabase {
+fun getMangaAppDatabase(applicationContext: Context): MangaAppDatabase {
     return Room.databaseBuilder(
         applicationContext,
         MangaAppDatabase::class.java, "manga-app-database"
