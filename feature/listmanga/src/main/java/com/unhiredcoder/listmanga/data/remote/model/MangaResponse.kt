ac38f96f42c0427b20bc648b8726e7d4d65a6883
PRATYUSH TIWARI <42827589+UnHired-Coder@users.noreply.github.com>
2025-03-21 22:03:29 +0530

    adds: response validator

--- feature/listmanga/src/main/java/com/unhiredcoder/listmanga/data/remote/model/MangaResponse.kt
+++ feature/listmanga/src/main/java/com/unhiredcoder/listmanga/data/remote/model/MangaResponse.kt
@@ -9,29 +9,50 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class MangaResponse(
-    val id: String,
-    @SerialName("image")
-    val imageUrl: String,
-    val score: Double,
-    val popularity: Long,
-    val title: String,
-    val publishedChapterDate: Long,
-    val category: String
-)
+    val id: String?,
+    @SerialName("image") val imageUrl: String?,
+    val score: Double?,
+    val popularity: Long?,
+    val title: String?,
+    val publishedChapterDate: Long?,
+    val category: String?
+) {
+    fun MangaResponse.validateMangaResponse() {
+        var invalidResponse: String? = null
+        if (id == null) invalidResponse = "Invalid ID"
+
+        if (imageUrl == null) invalidResponse = "Invalid imageUrl"
+
+        if (score == null) invalidResponse = "Invalid score"
+
+        if (popularity == null) invalidResponse = "Invalid popularity"
+
+        if (title == null) invalidResponse = "Invalid title"
+
+        if (publishedChapterDate == null) invalidResponse = "Invalid publishedChapterDate"
+
+        if (category == null) invalidResponse = "Invalid category"
+
+        invalidResponse?.let {
+            throw Throwable(invalidResponse)
+        }
+    }
+}
 
 fun MangaResponse.mapToMangaEntity(): MangaEntity {
+    validateMangaResponse()
     return MangaEntity(
-        id = id,
-        imageUrl = imageUrl,
-        score = score,
-        popularity = popularity,
-        title = title,
+        id = id!!,
+        imageUrl = imageUrl!!,
+        score = score!!,
+        popularity = popularity!!,
+        title = title!!,
         publishedChapterDate = publishedChapterDate.let {
-            val dateTime = Instant.fromEpochSeconds(publishedChapterDate)
+            val dateTime = Instant.fromEpochSeconds(publishedChapterDate!!)
                 .toLocalDateTime(TimeZone.currentSystemDefault())
             "${dateTime.dayOfMonth}-${dateTime.monthNumber}-${dateTime.year}"
         },
-        category = category,
+        category = category!!,
         isFavourite = false,
         isReadByUser = false
     )
