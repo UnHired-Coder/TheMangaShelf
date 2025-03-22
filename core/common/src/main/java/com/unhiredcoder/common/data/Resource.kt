04cf34cc0a14b44caf619a353f7534136957043e
PRATYUSH TIWARI <42827589+UnHired-Coder@users.noreply.github.com>
2025-03-22 16:14:16 +0530

    removes: redundant copyWith method, no longer needed

--- core/common/src/main/java/com/unhiredcoder/common/data/Resource.kt
+++ core/common/src/main/java/com/unhiredcoder/common/data/Resource.kt
@@ -4,9 +4,5 @@ sealed class Resource<out T>(val data: T?) {
     class Idle<T>(currentData: T? = null) : Resource<T>(currentData)
     class Loading<T>(currentData: T?) : Resource<T>(currentData)
     class Failure<T>(currentData: T?, val errorMessage: Throwable) : Resource<T>(currentData)
-    data class Success<T>(val updatedData: T) : Resource<T>(updatedData) {
-        fun copyWith(newData: T?): Success<T> {
-            return Success(newData ?: data?:updatedData)
-        }
-    }
+    data class Success<T>(val updatedData: T) : Resource<T>(updatedData)
 }
