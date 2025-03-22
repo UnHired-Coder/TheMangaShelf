cc64bbf15270862ff046e13f822e882e3af3612e
PRATYUSH TIWARI <42827589+UnHired-Coder@users.noreply.github.com>
2025-03-22 15:48:07 +0530

    updates: repository implementation updated, code cleanup

--- feature/listmanga/src/main/java/com/unhiredcoder/listmanga/data/MangaRepository.kt
+++ feature/listmanga/src/main/java/com/unhiredcoder/listmanga/data/MangaRepository.kt
@@ -1,59 +1,38 @@
 package com.unhiredcoder.listmanga.data
 
-import com.unhiredcoder.database.model.MangaEntity
-import com.unhiredcoder.listmanga.data.remote.model.MangaResponse
 import com.unhiredcoder.listmanga.data.remote.model.mapToMangaEntity
-import com.unhiredcoder.listmanga.domain.model.MangaModel
-import com.unhiredcoder.listmanga.domain.model.mapToMangaModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
+import com.unhiredcoder.listmanga.domain.model.mapToMangaDomainModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.map
 
 class MangaRepository(
     private val mangaRemote: IMangaC.Remote,
-    private val mangaLocal: IMangaC.Local
+    mangaLocal: IMangaC.Local
 ) : IMangaC.Repository {
-    override fun getMangaList(): Flow<List<MangaModel>> {
-        return combine(
-            mangaRemote.getMangaList(),
-            mangaLocal.getMangaList()
-        ) { remoteMangasList, localMangasList ->
-            mergeMangaLists(remoteMangasList, localMangasList)
+
+    private val localMangaFlow = mangaLocal.getMangaList()
+
+    override fun getMangaListLocal(): Flow<List<MangaDataModel>> {
+        return localMangaFlow.map { mangaList ->
+            mangaList.map { mangaEntity ->
+                mangaEntity.mapToMangaDataModel()
+            }
         }
+    }
+
+    override suspend fun getMangaListRemote(): Flow<List<MangaDataModel>> {
+        return mangaRemote.getMangaList().map { mangaList ->
+            mangaList.map { mangaResponse ->
+                mangaResponse.mapToMangaEntity().mapToMangaDataModel()
+            }
+        }
+    }
+
+    override fun updateManagaList(mangaDataModels: List<MangaDataModel>) {
+        mangaDataModels.map { it.mapToMangaDomainModel()}
     }
 
     override suspend fun markMangaFavourite(mangaId: String): Boolean {
         return true
-    }
-
-    private fun mergeMangaLists(
-        remoteList: List<MangaResponse>,
-        localList: List<MangaEntity>
-    ): List<MangaModel> {
-        return remoteList.map { remoteMangaItem ->
-            val remoteMangaEntity = remoteMangaItem.mapToMangaEntity()
-
-            val localMangaEntity =
-                localList.find { localMangaItem ->
-                    localMangaItem.id == remoteMangaEntity.id
-                } ?: remoteMangaEntity
-
-            localMangaEntity.copy(
-                imageUrl = remoteMangaEntity.imageUrl,
-                title = remoteMangaEntity.title,
-                publishedChapterDate = remoteMangaEntity.publishedChapterDate,
-                score = remoteMangaEntity.score,
-                popularity = remoteMangaEntity.popularity,
-                category = remoteMangaEntity.category,
-            )
-        }.also { mergeEntities ->
-            CoroutineScope(Dispatchers.IO).launch {
-                mangaLocal.updateMangaList(mergeEntities)
-            }
-        }.map {
-            it.mapToMangaModel()
-        }
     }
 }
